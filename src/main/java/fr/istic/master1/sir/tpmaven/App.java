e1b78c54a91f8f4dcbf0d469e27877a892e252a0
thiernousmansow <sowrennes1@yahoo.fr>
2015-01-14 20:31:00 +0100

    mon tp

---
+++ src/main/java/fr/istic/master1/sir/tpmaven/App.java
@@ -0,0 +1,14 @@
+package fr.istic.master1.sir.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	/*ouse msges pour afficher*/
+        System.out.println( "Hello World!" );
+    }
+}
